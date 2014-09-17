5595e4780e103eb447a3ca747629b9a53d1d2cf2
Kevin Hodges <kevin.hodges@holidayextras.com>
2014-09-17 10:01:31 +0100

    more boilerplate tweaks

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function( grunt ) {
 
 	// load tasks from the specified grunt plugins...
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-jscs-checker' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
 
 	// register task alias'
 	grunt.registerTask( 'default', ['jshint', 'jscs'] );
