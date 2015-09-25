c0e5da78e83f355f3e59ed3ede83a5e35f688c5d
Rob Johnson <rob.johnson@holidayextras.com>
2015-09-25 16:03:58 +0100

    Release check

---
+++ checkRelease.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+APP_VERSION="`cat package.json | jq '.version' | tr -d '"' `"
+RELEASE=`curl --user "${GHUSER}:${GHPASS}" https://api.github.com/repos/holidayextras/revolver/releases/latest | jq '.tag_name' | tr -d '"' | tr -d 'v'`
+
+echo "Current Release: $RELEASE"
+echo "Local Version: $APP_VERSION"
+
+LOCALVERSION=(${APP_VERSION//./ })
+REMOTEVERSION=(${RELEASE//./ })
+
+if [ ${LOCALVERSION[0]} -lt ${REMOTEVERSION[0]} ]; then
+	exit 1
+fi
+
+if [ ${LOCALVERSION[0]} -le ${REMOTEVERSION[0]} ] && [ ${LOCALVERSION[1]} -lt ${REMOTEVERSION[1]} ]; then
+	exit 1
+fi
+
+if [ ${LOCALVERSION[0]} -le ${REMOTEVERSION[0]} ] && [ ${LOCALVERSION[1]} -le ${REMOTEVERSION[1]} ] && [ ${LOCALVERSION[2]} -le ${REMOTEVERSION[2]} ]; then
+	exit 1
+fi
+exit 0
